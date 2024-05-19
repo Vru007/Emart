31e26a5941e73af9b8d28e22fcf662a5635c9418
Vru007 <vrushik7143work@gmail.com>
2024-05-19 15:55:31 +0530

    prductlist

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import  {ProductList} from './features/product-list/ProductList';
+function App() {
+  return (
+    <div className="App">
+      <ProductList/>
+    </div>
+  );
+}
+
+export default App;
